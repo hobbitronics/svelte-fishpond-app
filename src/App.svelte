d0341896560e7aa4bccde1a4c5d89133bf9bbe8b
hobbitronics <miguel.dw@gmail.com>
2021-08-02 21:08:41 -0400

    indentation and spacing

--- src/App.svelte
+++ src/App.svelte
@@ -31,51 +31,49 @@
 
 <OtherPage bind:toggle>
 	<!-- <TopAppBar on:nav={openMenu} bgColorIsVariant>{appName}</TopAppBar> -->
-
-<Page title="test page" layout="grid" >
-	<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-		<Page.Subheader>This is a template for ui-components</Page.Subheader>
-		
-		<Progress.Linear value='.2'/>
-	
-		<p class='fs-12 gray'>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	</div>
-
-	<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-		
-		<Card secondary="secondary text">
-			<p class="text-align-center">This is a card</p>
-
-			<Badge bordered color="#FE1234">M</Badge>
-
-			<div class="flex justify-center flex-wrap">
-				{#each topics as topic}
-					<StaticChip bgColor='yellow'>{topic}</StaticChip>
-				{/each}
-			</div>
-
-			<Form>
-				Form
-				<TextField label={'Textfield'} bind:value={text} />
-			</Form>
+	<Page title="test page" layout="grid" >
+		<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+			<Page.Subheader>This is a template for ui-components</Page.Subheader>
 			
-			<span slot="actions">
-				<Button raised on:click={clickHandler}>add a chip</Button>
-				<Button raised on:click={() => toggle = !toggle}>open drawer</Button>
-				<Checkbox label='check this out' bind:checked={checked} uppercase/>
-			</span>
-		</Card>
-
-	</div>
+			<Progress.Linear value='.2'/>
 		
-	<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-		<CustomCard buttons={[{label: 'go to ui-components', url: "https://github.com/silinternational/ui-components#readme"}]} icon="face" title='Title goes here' alt='graphic' src="">
-			Add some text content here.
-		</CustomCard>
-	</div>
-		
-	</Page>
+			<p class='fs-12 gray'>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+		</div>
 
+		<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+			
+			<Card secondary="secondary text">
+				<p class="text-align-center">This is a card</p>
+
+				<Badge bordered color="#FE1234">M</Badge>
+
+				<div class="flex justify-center flex-wrap">
+					{#each topics as topic}
+						<StaticChip bgColor='yellow'>{topic}</StaticChip>
+					{/each}
+				</div>
+
+				<Form>
+					Form
+					<TextField label={'Textfield'} bind:value={text} />
+				</Form>
+				
+				<span slot="actions">
+					<Button raised on:click={clickHandler}>add a chip</Button>
+					<Button raised on:click={() => toggle = !toggle}>open drawer</Button>
+					<Checkbox label='check this out' bind:checked={checked} uppercase/>
+				</span>
+			</Card>
+
+		</div>
+			
+		<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+			<CustomCard buttons={[{label: 'go to ui-components', url: "https://github.com/silinternational/ui-components#readme"}]} icon="face" title='Title goes here' alt='graphic' src="">
+				Add some text content here.
+			</CustomCard>
+		</div>
+			
+		</Page>
 
 	</OtherPage>
 
